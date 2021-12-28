99f468d6da92cb0bb01d99436d2dca4af82f65f6
luuisss1598 <39506329+luuisss1598@users.noreply.github.com>
2021-12-27 20:17:36 -0800

    Add files via upload
    send emails via emailJs

---
+++ reference website/assets/js/contactForm.js
@@ -0,0 +1,19 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init('user_tiJOs8dQgqsQ9M1gD9508');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_tailorEd_mhg9gkn', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
