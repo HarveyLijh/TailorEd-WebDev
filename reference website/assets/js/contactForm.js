a4e685a6f706fca94f54da32c610a6e2ba6d46c5
luuisss1598 <39506329+luuisss1598@users.noreply.github.com>
2022-01-30 18:27:25 -0800

    Update contactForm.js
    added auto reply, changed subject line, and fixed TailorEd email

--- reference website/assets/js/contactForm.js
+++ reference website/assets/js/contactForm.js
@@ -9,7 +9,7 @@ window.onload = function() {
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_tailorEd_mhg9gkn', this)
+        emailjs.sendForm('service_z5ail0n', 'template_x8j7lvn', this)
             .then(function() {
                 console.log('SUCCESS!');
                 let form = document.getElementById('contact-form');
